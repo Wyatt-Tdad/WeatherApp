46d1532c5b7c7267cee98f6bef3796b1bf6c5460
Wyatt Trinidad <wyatt.tdad@gmail.com>
2023-05-04 00:12:26 -0500

    update

--- src/main/java/com/tts/weatherapp/Request.java
+++
@@ -1,8 +0,0 @@
-package com.tts.weatherapp;
-
-import lombok.Data;
-
-@Data
-public class Request {
-    private String zipCode;
-}
--- src/main/java/com/tts/weatherapp/WeatherController.java
+++ src/main/java/com/tts/weatherapp/WeatherController.java
@@ -4,23 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping
+    @GetMapping("/")
     public String getIndex(Model model) {
-        model.addAttribute("request", new Request());  
+        Response response = weatherService.getForecast("43210");
+        System.out.println(response);
+        model.addAttribute("data", response);
         return "index";
-    }
-
-    @PostMapping
-    public String postIndex(Request request, Model model) {
-        Response data = weatherService.getForecast(request.getZipCode());
-        model.addAttribute("data", data);
-        return "index";
-    }
-    }
+    }}
