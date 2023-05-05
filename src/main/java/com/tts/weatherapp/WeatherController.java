3458f9d0d90ce39bb8653f541110360c50c916d9
Wyatt Trinidad <wyatt.tdad@gmail.com>
2023-05-04 19:38:30 -0500

    finsihed

--- src/main/java/com/tts/weatherapp/WeatherController.java
+++ src/main/java/com/tts/weatherapp/WeatherController.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping
-public String getIndex(Model model) {
-    Response response = weatherService.getForecast("43210");
-    System.out.println(response);
-    model.addAttribute("data", response);
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        model.addAttribute("request", new Request());  
+        return "index";
+    }
+
+    @PostMapping("/")
+public String postIndex(Request request, Model model) {
+    Response data = weatherService.getForecast(request.getZipCode());
+    model.addAttribute("data", data);
     return "index";
 }}
